194b8d523b512d07404b91bf8c126ff1136e92a6
Husnain Ali <husnainzulfiqarr@gmail.com>
2023-12-10 14:36:47 +0500

    Extinguisher working

--- Vr Emergency Response Training/Assets/PROJECT/Scripts/ExtinguisherWorking.cs
+++ Vr Emergency Response Training/Assets/PROJECT/Scripts/ExtinguisherWorking.cs
@@ -7,15 +7,35 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ExtinguisherWorking : MonoBehaviour
 {
+
+    [SerializeField] ParticleSystem smokeParticle; 
+
     private void Start()
     {
         XRGrabInteractable grabbale = this.GetComponent<XRGrabInteractable>();
+        
         grabbale.activated.AddListener(StartParticles);
+        
+        grabbale.deactivated.AddListener(StopParticles);
+        grabbale.selectExited.AddListener(StopParticles);
+
     }
 
-    public void StartParticles(ActivateEventArgs arg)
+    public void StartParticles(BaseInteractionEventArgs arg)
     {
-            
+        if (arg.interactorObject is XRDirectInteractor)
+        {
+            Debug.Log("EXTRACTED");
+            smokeParticle.Play();
+        }
+        Debug.Log("SMOKE STARTED");
+
     }
 
+    public void StopParticles(BaseInteractionEventArgs arg)
+    {
+        Debug.Log("REMOVED");
+
+        smokeParticle.Stop();
+    }
 }
