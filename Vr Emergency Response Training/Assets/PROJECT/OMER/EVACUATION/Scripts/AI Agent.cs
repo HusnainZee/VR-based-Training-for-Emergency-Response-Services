d547e9238f8112e52c673f0af1203788ce56c2fd
omernawaz29 <omarnawaz29@gmail.com>
2024-04-28 18:27:58 +0500

    Basic structure for Evacuation implemented
    Added ragdoll, added lady to put mask on. Going forward, adding UI to instruct, adding fail cases

---
+++ Vr Emergency Response Training/Assets/PROJECT/OMER/EVACUATION/Scripts/AI Agent.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIAgent : MonoBehaviour
+{
+    [SerializeField] Transform InjuredPosition;
+    private NavMeshAgent agentRef;
+    private Animator anim;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        agentRef = GetComponent<NavMeshAgent>();
+        RunToInjured();
+    }
+
+    public void RunToInjured()
+    {
+        agentRef.SetDestination(InjuredPosition.position);
+        anim.SetTrigger("Run");
+    }
+
+    void Crouch()
+    {
+        //agentRef.baseOffset = -0.5f;
+        anim.SetTrigger("Crouch");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Injured"))
+        {
+            
+            Crouch();
+        }
+    }
+
+
+
+}
